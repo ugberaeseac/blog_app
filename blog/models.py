41e691be45f388665ef58791117d700db7fcd9b2
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-10-20 06:34:45 -0400

    feat: implemented CRUD operations on blog posts for users and pagination

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -10,3 +11,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+    def get_absolute_url(self):
+        return reverse('blog-detail', kwargs={'pk': self.pk})
+
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    #path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('blog/new', PostCreateView.as_view(), name='blog-create'),
+    path('blog/<int:pk>/', PostDetailView.as_view(), name='blog-detail'),
+    path('blog/<int:pk>/update', PostUpdateView.as_view(), name='blog-update'),
+    path('blog/<int:pk>/delete', PostDeleteView.as_view(), name='blog-delete'),
     path('about/', views.about, name='blog-about'),
 ]
