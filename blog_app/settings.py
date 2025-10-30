aaedc0f0b1a5fb2c675568e7f7e2c9562a40306e
UGBERAESE CHARLES A <ugberaeseac@gmail.com>
2025-10-30 17:25:22 -0400

    fixing admin static files

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -137,9 +137,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
